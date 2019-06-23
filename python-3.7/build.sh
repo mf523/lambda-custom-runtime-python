a7679b443d751d20fbfe610f7111d165ef2777de
Laurence MIAO <mf523@outlook.com>
2019-06-23 07:17:27 +0000

    asyncio is built-in

--- python-3.7/build.sh
+++ python-3.7/build.sh
@@ -3,9 +3,10 @@
 docker build . -t local/build-python3.7
 docker run --rm -iv${PWD}:/host-volume local/build-python3.7 sh -s <<EOF
 cd /opt
-zip -r /build/python37.zip .
-cp -va /build/python37.zip /host-volume
+zip -r /build/python.zip .
+cp -va /build/python.zip /host-volume
 cd /build/python_libs
 zip -r /build/python_libs.zip .
 cp -va /build/python_libs.zip /host-volume
+
 EOF
