21546ee7641478e994d2230bfe240293439f236f
Laurence MIAO <mf523@outlook.com>
2019-06-23 02:53:30 +0000

    boto3

--- python-3.7/publish.sh
+++ python-3.7/publish.sh
@@ -1,1 +1,2 @@
 aws lambda publish-layer-version --layer-name my-python37-layer --description "My python-3.7" --zip-file fileb://python37.zip --compatible-runtimes provided
+aws lambda publish-layer-version --layer-name my-python37-libs-layer --description "My python-3.7-libs" --zip-file fileb://python_libs.zip --compatible-runtimes provided
