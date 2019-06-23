61ac435fbd04dc863796bdc153501b932aa8e1d3
Laurence MIAO <mf523@outlook.com>
2019-06-23 02:39:53 +0000

    python libs

--- python-3.7/build.sh
+++ python-3.7/build.sh
@@ -3,6 +3,9 @@
 docker build . -t local/build-python3.7
 docker run --rm -iv${PWD}:/host-volume local/build-python3.7 sh -s <<EOF
 cd /opt
-zip -r ../python37.zip .
-cp -va /python37.zip /host-volume
+zip -r /build/python37.zip .
+cp -va /build/python37.zip /host-volume
+cd /build/python_libs
+zip -r /build/python_libs.zip .
+cp -va /build/python_libs.zip /host-volume
 EOF
